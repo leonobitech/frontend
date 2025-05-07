71b03f9c89d7f1f94998e262cbf5271827b90221
leonobitech <leonobitech@gmail.com>
2025-05-07 10:00:56 -0300

    Revert "🔗 Add pages para navegación de la navbar"
    This reverts commit 8f022fb94bc3450dcbe0ad49caf7e04437701ac9.

--- app/(protected)/podcasts/my-podcasts/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MyPodcastsPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8">My Podcasts</h1>
-    </div>
-  );
-}
--- app/(protected)/projects/my-projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MyProjectsPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8">My Projects</h1>
-    </div>
-  );
-}
